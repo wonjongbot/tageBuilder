c36be61fbedf268d4945ba0cdeef722e5e2c5cd4
wonjongbot <wonjongbot@gmail.com>
2024-12-24 20:57:02 +0900

    custom methods for tag and idx calculation

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-DEBUG = 0
+import custom_algo
 
 TRACE_DIR = "/home/wonjongbot/rv32-OoO-SoC/third-party/wi12_proj1/src/"
 
@@ -7,3 +7,11 @@ REPORT_DIR = "/home/wonjongbot/tageBuilder/reports/"
 SPEC_DIR = "/home/wonjongbot/tageBuilder/configs/"
 
 READ_BATCH = True
+
+SPEC_NAME = "tage_custom"
+
+MIX_PATH_HISTORY_METHOD = custom_algo.mix_path_history_adv
+
+#TAG_CALC_METHOD = custom_taggedComp_tag
+
+#import types = custom_taggedComp_idx
