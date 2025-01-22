227f92e866632d522ccbe6226edc926f56655014
wonjongbot <13744405+wonjongbot@users.noreply.github.com>
2025-01-22 21:03:19 +0900

    setup file resolves directory issues

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
 import gdown
 import tarfile
+import os
+
+# get current working directory
+cwd = os.getcwd()
 
 # CBP env folder (from craymichael/CBP-16-Simulation @ github)
 cbp_url = 'https://drive.google.com/drive/folders/1VAdmqdOEFLvnRKkQQidxvGJA_C6S2RWo?usp=drive_link'
-
 gdown.download_folder(cbp_url, quiet=False, use_cookies=False)
 
-# Extract evaluation traces
-evaltraces = '/Users/wonjongbot/tageBuilder/CBP16 Data/evaluationTraces.Final.tar'
-extract_path = '/Users/wonjongbot/tageBuilder/CBP16 Data/'
 
-with tarfile.open(evaltraces, "r:") as tar:
-    tar.extractall(path=extract_path)
+# Extract evaluation traces
+evaltraces_dir = os.path.join(cwd, 'CBP16 Data/evaluationTraces.Final.tar')
+extracted_dir = os.path.join(cwd, 'CBP16 Data/')
+
+with tarfile.open(evaltraces_dir, "r:") as tar:
+    # tar.extractall(path=extracted_dir)
+    print(f'Trace extracted to {extracted_dir}')
+
+
+# setup directories used in sim
+coredir = os.path.join(cwd, 'tagebuilder_core')
+
+
+# setup settings to resolve directory dependencies
+settingfile_dir = os.path.join(cwd, 'tagebuilder_core/settings.py')
+
+settings_components = {
+    'CBP16_TRACE_DIR': os.path.join(extracted_dir, 'evaulationTraces'),
+    'REPORT_DIR': os.path.join(cwd, 'reports'),
+    'SPEC_DIR': os.path.join(cwd, 'configs')
+}
+
+with open(settingfile_dir, 'w') as f:
+    for k, v in settings_components.items():
+        f.write(f'{k} = \'{v}\'\n')
--- tagebuilder_core/settings.py
+++ tagebuilder_core/settings.py
@@ -1,20 +1,3 @@
-from . import custom_algo
-
-TRACE_DIR = "/home/wonjongbot/rv32-OoO-SoC/third-party/wi12_proj1/src/"
-CBP16_TRACE_DIR = "/home/wonjongbot/tageBuilder/cbp16/traces/"
-
-REPORT_DIR = "/home/wonjongbot/tageBuilder/reports/"
-
-SPEC_DIR = "/home/wonjongbot/tageBuilder/configs/"
-
-READ_BATCH = True
-
-SPEC_NAME = "bimodal"
-
-MIX_PATH_HISTORY_METHOD = custom_algo.mix_path_history_adv
-
-PROFILE = True
-
-#TAG_CALC_METHOD = custom_taggedComp_tag
-
-#import types = custom_taggedComp_idx
+CBP16_TRACE_DIR = '/Users/wonjongbot/tageBuilder/CBP16 Data/evaulationTraces'
+REPORT_DIR = '/Users/wonjongbot/tageBuilder/reports'
+SPEC_DIR = '/Users/wonjongbot/tageBuilder/configs'
